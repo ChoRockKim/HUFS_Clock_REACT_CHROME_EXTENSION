e3ddec7a73ffde8c5bc939820b9ac1fabff0daa2
김태헌 <daejincnc2@gmail.com>
2026-01-17 22:58:27 +0900

    잡다한 오류 수정

--- src/components/CurrentTime/CurrentTime.jsx
+++ src/components/CurrentTime/CurrentTime.jsx
@@ -15,7 +15,7 @@ export default function CurrentTime() {
     return (
         <>
         <div className="current-time-container">
-        <div style={{marginBottom: '5px'}}>현재시각</div>
+        <div className="current-time-header">현재시각</div>
         <div>{curTime.toLocaleString()}</div>
         </div>
         </>
